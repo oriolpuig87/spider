8cc23bee728e8699ccec8290b04f39cd9a5cdfbc
oriolpuig87 <45533417+oriolpuig87@users.noreply.github.com>
2018-12-02 12:43:38 +0100

    Update url.py

--- seoscraper/spiders/url.py
+++ seoscraper/spiders/url.py
@@ -6,11 +6,10 @@ from seoscraper.items import UrlscraperItem
 
 class UrlSpider(CrawlSpider):
     name = "url"
-    allowed_domains = ["example.com"]
+    allowed_domains = ["oriolpuig.es"]
     handle_httpstatus_list = [300, 301, 302, 303, 304, 305, 306, 307, 308, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 422, 426, 449, 451, 500, 501, 502, 503, 504, 505, 509]
     start_urls = (
-        'http://www.example.com/index.aspx',
-        'http://www.example.com/en/index.php'
+        'https://oriolpuig.es'
     )
 
     rules = (
